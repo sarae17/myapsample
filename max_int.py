b3069432dd276cd253388e076ceb1288ff92e467
sara <sarae17@ru.is>
2018-09-04 14:29:08 +0000

    fixed folder structure

---
+++ max_int.py
@@ -0,0 +1,12 @@
+#Then start implementing your algorithm
+#During your implementation, 
+# make sure you do git status, git add, and git commit regularly.
+#When you have finished your implementation:
+#Create a public repository on github
+#Follow the instructions under "push an existing repository from the command line"
+#Push your changes to the remote repo with: git push
+#Inspect your commits on github
+
+num_int = int(input("Input a number: "))    # Do not change this line
+# Fill in the missing code
+print("The maximum is", max_int)    # Do not change this line
