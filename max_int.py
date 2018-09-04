ad7b76424e179ea5f710a3ee26c0eb06b24228bd
sara <sarae17@ru.is>
2018-09-04 15:21:08 +0000

    Algorithm written for max_int

--- max_int.py
+++ max_int.py
@@ -1,12 +1,10 @@
-#Then start implementing your algorithm
-#During your implementation, 
-# make sure you do git status, git add, and git commit regularly.
-#When you have finished your implementation:
-#Create a public repository on github
-#Follow the instructions under "push an existing repository from the command line"
-#Push your changes to the remote repo with: git push
-#Inspect your commits on github
-
 num_int = int(input("Input a number: "))    # Do not change this line
 # Fill in the missing code
+max_int = num_int
+
+while num_int > 0:
+   num_int = int(input("Input a number:"))
+   if num_int > max_int:
+       max_int = num_int
+
 print("The maximum is", max_int)    # Do not change this line
